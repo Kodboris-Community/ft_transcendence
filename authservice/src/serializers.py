74f6d16631731a0a15e1290a992b8946dbd69e4b
Kingjaiyee <victordurojaiye2000@gmail.com>
2025-05-15 00:48:48 +0100

    frist push

--- authservice/src/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-
-class GenerateTokenSerializer(serializers.Serializer):
-    user_id = serializers.CharField(required=True)
-
-
-class RefreshTokenSerializer(serializers.Serializer):
-    refresh_token = serializers.CharField(required=True)
